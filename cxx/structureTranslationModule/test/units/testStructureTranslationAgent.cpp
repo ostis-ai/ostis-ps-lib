3aaa43c7639760e225dd6fe30f7ee2016fd505ff
Andy <irarybakova90@gmial.com>
2024-08-23 14:37:21 +0300

    fix: update tests

--- cxx/structureTranslationModule/test/units/testStructureTranslationAgent.cpp
+++ cxx/structureTranslationModule/test/units/testStructureTranslationAgent.cpp
@@ -36,6 +36,7 @@ namespace StructureTranslationAgent
 ScsLoader loader;
 std::string const & TEST_FILES_DIR_PATH = STRUCTURE_TRANSLATION_MODULE_TEST_SRC_PATH "/testStructures/StructureTranslation/";
 std::string const & TEST_QUESTION_NODE_ALIAS = "test_question_node";
+std::string const & TEST_ANSWER_NODE_ALIAS = "test_answer_node";
 std::string const & TEST_STRUCTURE_ALIAS = "test_structure";
 int const WAIT_TIME = 3000;
 
@@ -54,7 +55,7 @@ void deinitializeClasses()
   SC_AGENT_UNREGISTER(structureTranslationModule::StructureTranslationAgent);
 }
 
-void testTranslator(ScMemoryContext & context, StructureTranslator & translator, std::string fileName, std::vector<std::string> answerPhrases)
+void testTranslator(ScMemoryContext & context, StructureTranslator & translator, std::string fileName, std::vector<std::string> answerPhrases, bool alternative)
 {
   initializeClasses();
 
@@ -62,9 +63,16 @@ void testTranslator(ScMemoryContext & context, StructureTranslator & translator,
   ScAddr test_structure = context.HelperFindBySystemIdtf(TEST_STRUCTURE_ALIAS);
   std::string const & answer = translator.translate(test_structure).str();
 
-  for (std::string const & phrase : answerPhrases)
+  if (alternative)
   {
-    EXPECT_TRUE(answer.find(phrase) != std::string::npos);
+    EXPECT_TRUE(answer.find(answerPhrases[0]) != std::string::npos || answer.find(answerPhrases[1]) != std::string::npos);
+  }
+  else
+  {
+    for (std::string const & phrase : answerPhrases)
+    {
+      EXPECT_TRUE(answer.find(phrase) != std::string::npos);
+    }
   }
 
   deinitializeClasses();
@@ -75,7 +83,7 @@ TEST_F(StructureTranslationTest, TestFromConceptTranslator)
   std::vector<std::string> answerPhrases = {"красный это цвет", "яблоко это фрукт"};
   ScMemoryContext & context = *m_ctx;
   FromConceptTranslator translator(&context);
-  testTranslator(context, translator, "testFromConceptTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testFromConceptTranslator.scs", answerPhrases, false);
 }
 
 TEST_F(StructureTranslationTest, TestNrelInLinkTranslator)
@@ -83,47 +91,47 @@ TEST_F(StructureTranslationTest, TestNrelInLinkTranslator)
   std::vector<std::string> answerPhrases = {"яблоко имя Арнольд"};
   ScMemoryContext & context = *m_ctx;
   NrelInLinkTranslator translator(&context);
-  testTranslator(context, translator, "testNrelInLinkTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testNrelInLinkTranslator.scs", answerPhrases, false);
 }
 
 TEST_F(StructureTranslationTest, TestNrelInQuasybinaryLinkTranslator)
 {
-  std::vector<std::string> answerPhrases = {"человек синонимы хомо сапиенс", "человек синонимы людь"};
+  std::vector<std::string> answerPhrases = {"мужчина синонимы хомо сапиенс, людь,", "мужчина синонимы людь, хомо сапиенс,"};
   ScMemoryContext & context = *m_ctx;
   NrelInQuasybinaryLinkTranslator translator(&context);
-  testTranslator(context, translator, "testNrelInQuasybinaryLinkTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testNrelInQuasybinaryLinkTranslator.scs", answerPhrases, true);
 }
 
 TEST_F(StructureTranslationTest, TestNrelFromQuasybinaryLinkTranslator)
 {
-  std::vector<std::string> answerPhrases = {"яблоко декомпозиция мякоть", "яблоко декомпозиция кожура"};
+  std::vector<std::string> answerPhrases = {"яблоко декомпозиция мякоть, кожура,", "яблоко декомпозиция кожура, мякоть,"};
   ScMemoryContext & context = *m_ctx;
   NrelFromQuasybinaryLinkTranslator translator(&context);
-  testTranslator(context, translator, "testNrelFromQuasybinaryLinkTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testNrelFromQuasybinaryLinkTranslator.scs", answerPhrases, true);
 }
 
 TEST_F(StructureTranslationTest, TestNrelInQuasybinaryNodeTranslator)
 {
-  std::vector<std::string> answerPhrases = {"человек родители первый родитель", "человек родители второй родитель"};
+  std::vector<std::string> answerPhrases = {"человек родители первый родитель, второй родитель", "человек родители второй родитель, первый родитель"};
   ScMemoryContext & context = *m_ctx;
   NrelInQuasybinaryNodeTranslator translator(&context);
-  testTranslator(context, translator, "testNrelInQuasybinaryNodeTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testNrelInQuasybinaryNodeTranslator.scs", answerPhrases, true);
 }
 
 TEST_F(StructureTranslationTest, TestNrelFromQuasybinaryNodeTranslator)
 {
-  std::vector<std::string> answerPhrases = {"штука декомпозиция на части большая часть", "штука декомпозиция на части маленькая часть"};
+  std::vector<std::string> answerPhrases = {"штука декомпозиция на части большая часть, маленькая часть", "штука декомпозиция на части маленькая часть, большая часть"};
   ScMemoryContext & context = *m_ctx;
   NrelFromQuasybinaryNodeTranslator translator(&context);
-  testTranslator(context, translator, "testNrelFromQuasybinaryNodeTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testNrelFromQuasybinaryNodeTranslator.scs", answerPhrases, true);
 }
 
 TEST_F(StructureTranslationTest, TestNrelFromNodeTranslator)
 {
-  std::vector<std::string> answerPhrases = {"человек любит пицца", "человек любит есть пицца"};
+  std::vector<std::string> answerPhrases = {"мужчина любит пицца", "мужчина любит есть пицца"};
   ScMemoryContext & context = *m_ctx;
   NrelFromNodeTranslator translator(&context);
-  testTranslator(context, translator, "testNrelFromNodeTranslator.scs", answerPhrases);
+  testTranslator(context, translator, "testNrelFromNodeTranslator.scs", answerPhrases, false);
 }
 
 TEST_F(StructureTranslationTest, TestAllTranslators)
@@ -166,20 +174,16 @@ TEST_F(StructureTranslationTest, TestAllTranslators)
 
   EXPECT_TRUE(answer.find("яблоко имя Арнольд") != std::string::npos);
 
-  EXPECT_TRUE(answer.find("мужик синонимы хомо сапиенс") != std::string::npos);
-  EXPECT_TRUE(answer.find("мужик синонимы людь") != std::string::npos);
+  EXPECT_TRUE(answer.find("мужчина синонимы хомо сапиенс, людь,") != std::string::npos || answer.find("мужчина синонимы людь, хомо сапиенс,") != std::string::npos);
 
-  EXPECT_TRUE(answer.find("яблоко декомпозиция мякоть") != std::string::npos);
-  EXPECT_TRUE(answer.find("яблоко декомпозиция кожура") != std::string::npos);
+  EXPECT_TRUE(answer.find("яблоко декомпозиция мякоть, кожура,") != std::string::npos || answer.find("яблоко декомпозиция кожура, мякоть,") != std::string::npos);
 
-  EXPECT_TRUE(answer.find("человек родители первый родитель") != std::string::npos);
-  EXPECT_TRUE(answer.find("человек родители второй родитель") != std::string::npos);
+  EXPECT_TRUE(answer.find("человек родители первый родитель, второй родитель") != std::string::npos || answer.find("человек родители второй родитель, первый родитель") != std::string::npos);
 
-  EXPECT_TRUE(answer.find("штука декомпозиция на части большая часть") != std::string::npos);
-  EXPECT_TRUE(answer.find("штука декомпозиция на части маленькая часть") != std::string::npos);
+  EXPECT_TRUE(answer.find("штука декомпозиция на части большая часть, маленькая часть") != std::string::npos || answer.find("штука декомпозиция на части маленькая часть, большая часть") != std::string::npos);
 
-  EXPECT_TRUE(answer.find("человек любит пицца") != std::string::npos);
-  EXPECT_TRUE(answer.find("человек любит есть пицца") != std::string::npos);
+  EXPECT_TRUE(answer.find("мужчина любит есть пицца") != std::string::npos);
+  EXPECT_TRUE(answer.find("мужчина любит пицца") != std::string::npos);
 
   deinitializeClasses();
   for (const auto & translator : translators)
@@ -193,6 +197,7 @@ TEST_F(StructureTranslationTest, TestAllAgent)
 
   loader.loadScsFile(context, TEST_FILES_DIR_PATH + "testAgentOnAllTranslations.scs");
   ScAddr testQuestionNode = context.HelperFindBySystemIdtf(TEST_QUESTION_NODE_ALIAS);
+  ScAddr testAnswerNode = context.HelperFindBySystemIdtf(TEST_ANSWER_NODE_ALIAS);
   EXPECT_TRUE(testQuestionNode.IsValid());
 
   EXPECT_TRUE(utils::AgentUtils::applyAction(&context, testQuestionNode, WAIT_TIME));
@@ -200,9 +205,7 @@ TEST_F(StructureTranslationTest, TestAllAgent)
       scAgentsCommon::CoreKeynodes::question_finished_successfully,
       testQuestionNode,
       ScType::EdgeAccessConstPosPerm));
-  ScAddr const & answer = utils::IteratorUtils::getAnyByOutRelation(&context, testQuestionNode, scAgentsCommon::CoreKeynodes::nrel_answer);
-  ASSERT_TRUE(answer.IsValid());
-  ScAddr const & answerLink = utils::IteratorUtils::getAnyFromSet(&context, answer);
+  ScAddr const & answerLink = utils::IteratorUtils::getAnyFromSet(&context, testAnswerNode);
   ASSERT_TRUE(context.GetElementType(answerLink).IsLink());
 
   deinitializeClasses();
